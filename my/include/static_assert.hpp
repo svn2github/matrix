35c39a2f37798f8fafb73dba25fbd8bf38c6999e
hanji1984 <hanji1984@2159fae2-c2cd-11de-acfc-b766374499fb>
2009-10-27 13:51:16 +0000

    fixed a typo in typedef-ed type name: assersion_type -> assertion_type
    git-svn-id: http://matrix-library-in-cplusplus.googlecode.com/svn/trunk@4 2159fae2-c2cd-11de-acfc-b766374499fb

--- my/include/static_assert.hpp
+++ my/include/static_assert.hpp
@@ -2,12 +2,12 @@
 #define STATIC_ASSERT_HPP
 
 template <bool assertion> struct StaticAssert { };
-template<> struct StaticAssert<true> { typedef char assersion_type; };
+template<> struct StaticAssert<true> { typedef char assertion_type; };
 
 #define static_assert_concat_(a, b) a##b
 #define static_assert_concat(a, b) static_assert_concat_(a, b)
 #define static_assert(p) \
-    typedef StaticAssert<(p)>::assersion_type \
+    typedef StaticAssert<(p)>::assertion_type \
     static_assert_concat(static_assert_at_line_,__LINE__)
 
 
